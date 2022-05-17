a791c589172f140a3c62d3a553a218faa9e0dfa2
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-05-17 19:27:14 -0400

    Object and Array

--- Data_Structures.js
+++ Data_Structures.js
@@ -14,3 +14,20 @@ animalList.push("dog"); // adds a new element to an array (at the end)
 animalList.shift();// removes the first array element
 
 animalList.unshift("elephant"); // adds a new element to an array (at the beginning)
+
+//Object
+
+const user = {
+  name: "John",
+  age: 34,
+  hobby: "soccer",
+  isMarried: false,
+  spells: ["abrakadra", "shazam", "boo"],
+  shout: function() {
+    console.log("AHHHHHH!");
+  }
+}
+
+user.spells[0] // to access the array inside an object
+
+user.shout() // to invoke a method inside an object
