3f5d82e46093cec60ed982279c3873ab961648a6
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-05-17 18:48:48 -0400

    JavaScript Functions

---
+++ Functions.js
@@ -0,0 +1,17 @@
+//JAVASCRIPT FUNCTIONS
+//-----------------
+
+//Function Declaration
+function sayHello() {
+  console.log("Hello");
+}
+
+//Function expression
+const sayBye = function() {
+  console.log("Bye");
+}
+
+//Arrow function
+const multiply = (x, y) => {
+  return x * y;
+}
