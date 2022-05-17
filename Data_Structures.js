a34a708557fefb0314f73a4f615be273deea1197
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-05-17 19:11:38 -0400

    Array and Methods

---
+++ Data_Structures.js
@@ -0,0 +1,16 @@
+//Data Structures
+Array
+Object
+
+const animalList = ["tiger", "cat", "bear", "bird"];
+console.log(animalList[3]); // bird
+
+//Array Methods
+
+animalList.pop(); //removes the last element from an array
+
+animalList.push("dog"); // adds a new element to an array (at the end)
+
+animalList.shift();// removes the first array element
+
+animalList.unshift("elephant"); // adds a new element to an array (at the beginning)
