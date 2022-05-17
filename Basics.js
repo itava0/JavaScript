eccce1d82f92f30079afa1b90d3497a7d8c5dedb
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-05-17 13:08:29 -0400

    Going over JavaScript Data types

---
+++ Basics.js
@@ -0,0 +1,52 @@
+//JavaScript Data Types
+// 1. Number
+3 + 6;
+// 2. String
+"Hello word";
+// 3. Boolean 
+true;
+false;
+// 4. Undefined 
+// 5. Null 
+// 6. Symbol 
+// 7. Object 
+
+//JAVASCRIPT COMPARISONS
+//-----------------
+
+// !==
+// ===
+// >=
+// <=
+// >
+// <
+
+5 < 10;
+
+// //JAVASCRIPT VARIABLES
+// //-----------------
+// var
+// <!-- let (new in ECMAScript 6)-->  
+// <!-- const (new in ECMAScript 6)-->
+
+var a = true;
+
+// JAVASCRIPT CONDITIONALS
+// -----------------
+// if
+// else
+// else if
+// <!-- ternary operator -->
+// <!-- switch -->
+
+if(a) {
+  "Hello There";
+}
+
+
+// JAVASCRIPT LOGICAL OPERATORS
+// -----------------
+// &&
+// ||
+// !
+
