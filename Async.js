a8b14a72fd842238d1d3192c0dc6b45aea9ae6a4
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-05-28 18:17:41 -0400

    Done

--- Async.js
+++ Async.js
@@ -1,10 +1,33 @@
 //Async Await
 
 async function playerStart() {
-  await movePlayer(100, 'Left');
+  await movePlayer(100, "Left");
 }
 
 async function fetchUsers() {
-  const resp = await fetch('hahdhhd')
+  const resp = await fetch("hahdhhd");
   const data = await resp.json();
 }
+
+const getData = async function () {
+  try {
+    const [users, posts, albums] = await Promise.all(
+      urls.map((url) => {
+        fetch(url).then((resp) => resp.json());
+      })
+    );
+    console.log("users", users);
+    console.log("posts", posts);
+    console.log("albums", albums);
+  } catch (err) {
+    console.log("ooops", err);
+  }
+};
+
+const getData2 = async function () {
+  const arrayOfPromises = urls.map((url) => fetch(url));
+  for await (let request of arrayOfPromises) {
+    const data = await request.json();
+    console.log(data);
+  }
+};
